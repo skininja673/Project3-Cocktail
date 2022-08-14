b3222e5bd557ea428e0bef37932f9a2d95372cdc
Akalpit <skininja673@gmail.com>
2022-08-13 22:52:54 -0400

    checking if api is working or not-3

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
     const [cocktails, setCocktails] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('margarita');
+
     const fetchDrinks = async () => {
         const response = await fetch(url);
         const data = await response.json();
@@ -24,21 +26,22 @@ function App() {
             setCocktails(newCocktails);
         } else {
             setCocktails([]);
+            setSearchTerm('Margarita');
         }
         console.log(cocktails);
     };
 
     useEffect(() => {
         fetchDrinks();
-    }, []);
+    }, [searchTerm]);
 
     return (
         <div className="App">
             <h1>Welcome</h1>
             <h2>Testing the api link.....</h2>
             <h3>Please check the console log to see the data from api</h3>
-            {cocktails.map((cocktail) => {
-                return <img src={cocktail.image} alt="" />;
+            {cocktails.map((cocktail, index) => {
+                return <img src={cocktail.image} alt="" key={index} />;
             })}
         </div>
     );
