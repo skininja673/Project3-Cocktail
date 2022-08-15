afdce7444f6add755c8b47dab03f8e4f3e35ec5b
Akalpit <skininja673@gmail.com>
2022-08-15 17:46:34 -0400

    added error functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
 
 // import components
 import Navbar from './components/Navbar';
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="about" element={<About />} />
                     <Route path="cocktail/:id" element={<SingleCocktail />} />
+                    <Route path="*" element={<Error />} />
                 </Routes>
             </Router>
         </div>
